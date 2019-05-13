24635bae0f24570f41c3b5ccedb6105f1dfb20e7
Arnaud Auzou <arnaud.auzoudm@gmail.com>
2019-05-13 15:58:15 +0200

    quest Symfony 3

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/default", name="app_index")
+     */
+    public function index()
+    {
+        return $this->render('default.html.twig', [
+            'owner' => 'Nono',
+        ]);
+    }
+}
