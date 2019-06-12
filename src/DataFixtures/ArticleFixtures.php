10fd5f3096de2807fffa14503b8d1b10610a3788
Arnaud Auzou <arnaud.auzoudm@gmail.com>
2019-06-12 09:25:18 +0200

    quest 14 done

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker;
+use App\Entity\Article;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+class ArticleFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create('fr_FR');
+        for ($i = 0; $i < 50; $i++) {
+            $article = new Article();
+            $article->setTitle(mb_strtolower($faker->sentence()));
+            $article->setContent($faker->text(50));
+
+            $manager->persist($article);
+            $article->setCategory($this->getReference('categorie_' . rand(0, 4)));
+        }
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [CategoryFixtures::class];
+    }
+
+}
